6cc75ab6740099a2c1ba796651dc331ae3da4363
Mike Zhou <mikeweizhou@gmail.com>
2018-11-24 13:36:02 -0800

    Updated environment configs to add Walmart API Key and Endpoint URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+
+  "WALMART": {
+    // Walmart Open API KEY
+    // https://developer.walmartlabs.com/
+    "API_KEY": "pxwzmp27a72kgexvac7vpq9z",
+
+    // API Endpoints (URLs)
+    "API_ENDPOINTS": {
+      "TAXONOMY":   "https://api.walmartlabs.com/v1/taxonomy",
+      "PAGINATED":  "https://api.walmartlabs.com/v1/paginated/items",
+      "LOOKUP":     "https://api.walmartlabs.com/v1/items"
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  "WALMART": {
+    // Walmart Open API KEY
+    // https://developer.walmartlabs.com/
+    "API_KEY": "pxwzmp27a72kgexvac7vpq9z",
+
+    // API Endpoints (URLs)
+    "API_ENDPOINTS": {
+      "TAXONOMY":   "https://api.walmartlabs.com/v1/taxonomy",
+      "PAGINATED":  "https://api.walmartlabs.com/v1/paginated/items",
+      "LOOKUP":     "https://api.walmartlabs.com/v1/items"
+    }
+  }
 };
 
 /*
