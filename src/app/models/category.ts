7cb3df80e8d1a8e22ebe08b9d1af08904dc50338
Mike Zhou <mikeweizhou@gmail.com>
2018-11-25 10:49:09 -0800

    Updated Category model to reflect response from Walmart Taxonomy API

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,14 +1,17 @@
 /**
- * Walmart Category model.
- * (matches Taxonomy API response object)
+ * Category Model.
+ * Matches Walmart Taxonomy API response.
+ * https://developer.walmartlabs.com/docs/read/Taxonomy_API
  * 
  * Used for VS Code Intellisense.
  * JavaScript does not enforce this model.
  */
 export class Category {
+  // in Walmart documentation
   id: string;
   name: string;
-  path: string;
-
   children?: Category[];
+
+  // Not in Walmart documentation
+  path?: string;
 }
